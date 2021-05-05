fd59639fe64222fc1b5c84af7cbbc57d8dc674f9
alex olivares <alexander.b.olivares@gmail.com>
2021-05-05 17:53:46 -0500

    InputItem post request working

---
+++ src/components/InputItem.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export default function InputItem() {
+  const [description, setDescription] = useState("");
+
+  const handleUserInput = e => setDescription(e.target.value);
+  console.log(description);
+
+  const handleFormSubmit = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/items", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="display-4 text-center my-5">Our List</h1>
+      <form className="d-flex" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="add item"
+          className="form-control"
+          value={description}
+          onChange={handleUserInput}
+        ></input>
+        <button className="btn btn-success">add</button>
+      </form>
+    </>
+  );
+}
